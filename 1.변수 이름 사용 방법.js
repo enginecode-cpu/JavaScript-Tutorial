964597970cbbd72679858031ef3e389e85f57012
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-29 21:44:58 +0900

    변수 이름 사용 방법

---
+++ 1.변수 이름 사용 방법.js
@@ -0,0 +1,33 @@
+let val = 1;
+console.log(val);
+
+let name = '신짱구';
+console.log(name);
+
+// 같은 이름의 변수를 사용하면 Error
+// let name = 1;
+
+const a = 1;
+console.log(a);
+
+// 고정 값이기 때문에 다른 값을 넣으면 Error
+// a = 'name';
+// console.log(a);
+
+let n1 = 1;
+console.log(n1);
+
+let n2 = 4.5;
+console.log(n2);
+
+let text = 'Hello JavaScript';
+console.log(text);
+
+let bool = true;
+console.log(bool);
+
+let null_val = null;
+console.log(null_val);
+
+let undefined_val;
+console.log(undefined_val);
