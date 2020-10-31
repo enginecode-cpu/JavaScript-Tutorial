9bb6f4d8ed8406a798ea10f618e9930fe23a6cb0
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-31 20:18:47 +0900

    unshift를 앞으로 옮김

--- 9.배열을 이용한 Stack, Queue.js
+++ 9.배열을 이용한 Stack, Queue.js
@@ -13,16 +13,18 @@ const array2 = [1, 2, 3, 4, 5, 6, 7, 10];
 array2.pop(); // 10 삭제
 console.log(array2);
 
+console.log('-----');
+
 // Queue
+// unshift : 맨 앞에 원소를 집어넣기
+const numbers2 = [100, 21, 40, 32, 50];
+numbers2.unshift(90);
+console.log(numbers2);
+
+console.log('-----');
+
 // shift : 맨 앞의 원소를 빼기
 const numbers = [11, 12, 13, 14, 15, 30, 20, 10];
 const value = numbers.shift();
 console.log(value);
 console.log(numbers);
-
-console.log('-----');
-
-// unshift : 맨 앞에 원소를 집어넣기
-const numbers2 = [100, 21, 40, 32, 50];
-numbers2.unshift(90);
-console.log(numbers2);
